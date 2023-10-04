36f99edc1bf52141b9ead993d80df5d886299ba2
Iury Landin <iurylandim@unitins.br>
2023-10-04 00:54:32 -0300

    Refact: desativar logs do sequelize

--- helpers/database.js
+++ helpers/database.js
@@ -6,6 +6,7 @@ const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, proces
     dialect: "mysql",
     dialectModule: require('mysql2'),
     port: process.env.DB_PORT || 3306,
+    logging: false
 });
 
 module.exports = sequelize;
